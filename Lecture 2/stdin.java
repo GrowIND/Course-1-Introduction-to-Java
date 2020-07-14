2d2070ff3a447cf0032427849e52da76496c74f7
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-07-14 13:03:57 +0530

    Input in Java

---
+++ Lecture 2/stdin.java
@@ -0,0 +1,18 @@
+//GrowIND
+//Introduction to Java
+//Lecture 2: Program Structure in Java
+
+package com.company;
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args){
+    
+       int n;
+       Scanner scan = new Scanner(System.in);
+       n=scan.nextInt();
+       System.out.println(n);
+       
+    }
+}
