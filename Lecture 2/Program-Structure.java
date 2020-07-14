9a9a60533df77899baa81511448ab690b96a519d
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-07-14 10:18:05 +0530

    First Program in java

--- Lecture 2/Program-Structure.java
+++ Lecture 2/Program-Structure.java
@@ -1,3 +1,10 @@
 //GrowIND
 //Introduction to Java
 //Lecture 2: Program Structure in Java
+package com.company;
+
+class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+    }
+}
