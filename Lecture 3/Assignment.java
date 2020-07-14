a80366accfcff4aea6f1428ebab5cb2e67ef976d
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-07-14 22:37:14 +0530

    Solution to Assignment

---
+++ Lecture 3/Assignment.java
@@ -0,0 +1,18 @@
+//GrowIND
+//Introduction to Java
+//Lecture 3: Data type, Variable and Operators in Java
+
+package com.company;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Enter area of rectangle");
+      int a = scan.nextInt();
+      int b = scan.nextInt();
+      int area = a*b;
+      int paint=area*2;
+      System.out.println("Paint required will be " +paint+ "ml" );
+    }
+}
