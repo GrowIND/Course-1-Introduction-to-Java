3a8687a06df9e49f8241d641f3f6b3698fc0faa2
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-07-14 09:16:03 +0530

    Update OOP-Example.java

--- Lecture 1/OOP-Example.java
+++ Lecture 1/OOP-Example.java
@@ -1,3 +1,10 @@
 //GrowIND
 //Introduction to Java
 //Lecture 1: Introduction to Object Oriented Programming
+
+package com.company;
+
+class Vehicle{
+           int top_speed = 80, acc;
+           char color;
+        }
