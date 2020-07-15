a829e3736b90d8089ac0b20697f4d36fc4c7b971
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-07-15 10:10:40 +0530

    Ternary Operator Example

--- Lecture 4/Ternary Operator.java
+++ Lecture 4/Ternary Operator.java
@@ -1,3 +1,17 @@
 //GrowIND
 //Introduction to Java
 //Lecture 4: Control Statement I: if-else
+package com.company;
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the number you want to check");
+        int n=s.nextInt();
+        String str=new String();
+       str = n%2==0 ? "even":"odd";
+        System.out.println("The Number "+n+" is "+str);
+    }
+}
