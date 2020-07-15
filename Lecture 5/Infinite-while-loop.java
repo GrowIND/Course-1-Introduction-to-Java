2709cedf67c0ca187aea7949a2758f8e944db1ba
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-07-15 11:30:10 +0530

    While loop example 1

---
+++ Lecture 5/Infinite-while-loop.java
@@ -0,0 +1,15 @@
+//GrowIND
+//Introduction to Java
+//Lecture 5: Control Statement II: Loops
+
+package com.company;
+
+
+class Main {
+    public static void main(String[] args) {
+
+        int n=1;
+        while (n == 1)
+            System.out.println("hello");
+    }
+    }
