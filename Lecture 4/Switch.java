8b65766bcd4b9565045e0b7b9e976a5395075053
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-07-15 10:24:55 +0530

    Switch Case Example

--- Lecture 4/Switch.java
+++ Lecture 4/Switch.java
@@ -1,3 +1,43 @@
 //GrowIND
 //Introduction to Java
 //Lecture 4: Control Statement I: if-else
+package com.company;
+import java.util.Scanner;
+
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the week day number");
+        int week = s.nextInt();
+        String day;
+
+        switch (week) {
+            case 1:
+                day = "Sunday";
+                break;
+            case 2:
+                day = "Monday";
+                break;
+            case 3:
+                day = "Tuesday";
+                break;
+            case 4:
+                day = "Wednesday";
+                break;
+            case 5:
+                day = "Thursday";
+                break;
+            case 6:
+                day = "Friday";
+                break;
+            case 7:
+                day = "Saturday";
+                break;
+            default:
+                day = "Invalid day";
+                break;
+        }
+        System.out.println("The day is " + day);
+    }
+}
