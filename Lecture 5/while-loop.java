9e129651bb465add04b7ab95a339313303b87e5d
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-07-15 11:33:54 +0530

    Create while-loop.java

---
+++ Lecture 5/while-loop.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String args[]) {
+        int x = 10;
+
+        while( x < 20 ) {
+            System.out.print("value of x : " +x+"\n" );
+            x++;
+        }
+    }
+}
