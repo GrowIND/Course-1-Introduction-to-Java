25daaa7673179d7c0cf072218d36f4c04e20556d
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-07-18 00:30:06 +0530

    Basic Declaration and Initialization with printing

---
+++ Lecture 7/Example 1.java
@@ -0,0 +1,15 @@
+//GrowIND
+//Introduction to Java
+//Lecture 7: Arrays
+
+
+package com.company;
+
+class Main {
+
+    public static void main(String args[]){
+        int arr[]={33,3,4,5};
+
+        for(int i=0 ; i<arr.length;i++)
+            System.out.println(arr[i]);
+    }}
