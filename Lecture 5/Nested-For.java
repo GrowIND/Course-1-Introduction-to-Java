88d8239c4d685047cbc1a1668535663c24f0c523
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-07-17 09:49:04 +0530

    For Loops Example 4

---
+++ Lecture 5/Nested-For.java
@@ -0,0 +1,20 @@
+//GrowIND
+//Introduction to Java
+//Lecture 5: Control Statement II: Loops
+
+package com.company;
+
+
+public class Main
+{
+    public static void main(String args[]) {
+        for (int i = 1; i <= 5; ++i) {
+
+            System.out.println("Outer loop iteration " + i);
+
+            for (int j = 1; j <=2; ++j) {
+                System.out.println("i = " + i + "; j = " + j);
+            }
+        }
+    }
+}
