4b18d0b23b203e7aa00e80c91341e72cf1e24ae4
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-07-17 09:07:01 +0530

    Create For.java

---
+++ Lecture 5/For.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; ++i) {
+            System.out.println(i);
+        }
+    }
+}
