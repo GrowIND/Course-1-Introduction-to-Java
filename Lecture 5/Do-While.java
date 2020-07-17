f6ee028b8ee5ae796174528920b51266ac7ccdb3
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-07-17 09:02:58 +0530

    Do-while Example

---
+++ Lecture 5/Do-While.java
@@ -0,0 +1,15 @@
+//GrowIND
+//Introduction to Java
+//Lecture 5: Control Statement II: Loops
+
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+    }
+}
