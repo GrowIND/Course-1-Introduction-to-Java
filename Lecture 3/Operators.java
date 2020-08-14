a6a8bf4076f0af4e703c192817eeada31ee7e583
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-08-14 16:39:58 +0530

    Create Operators.java

---
+++ Lecture 3/Operators.java
@@ -0,0 +1,20 @@
+package com.company;
+
+class Main{
+    public static void main(String[] args){
+        byte a;
+        short b;
+        int c=5;
+        long d;
+
+        float i = 2;
+        double j;
+        float s= c/i;
+
+        char x;
+
+        boolean p;
+        System.out.print(s);
+    }
+
+}
