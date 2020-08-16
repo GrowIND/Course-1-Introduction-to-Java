cb83f6c73580e7d63357a9fa9353ee4d272bc30f
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-08-16 17:08:44 +0530

    Example

---
+++ Lecture 5/While2.java
@@ -0,0 +1,17 @@
+//GrowIND
+//Introduction to Java
+//Lecture 5: Control Statement II: Loops
+package com.company;
+
+
+public class Main {
+
+    public static void main(String args[]) {
+        byte x = 10;
+
+        while( x > 2 ) {
+            System.out.print("value of x : " +x+"\n" );
+            ++x;
+        }
+    }
+}
