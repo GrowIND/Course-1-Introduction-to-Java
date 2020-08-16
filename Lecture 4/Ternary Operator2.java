8e61aab9d5227a12f82499bccc7c3247e74699fe
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-08-16 16:28:08 +0530

    Create Ternary Operator2.java

---
+++ Lecture 4/Ternary Operator2.java
@@ -0,0 +1,12 @@
+
+package com.company;
+
+
+public class Main{
+    public static void main(String[] args){
+
+        int max, n = 250, m = 50;
+       max =  n > m ? n : m;
+        System.out.print(max);
+    }
+}
