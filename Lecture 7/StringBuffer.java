88a55f75905c59d2656fa13c3a2c40de3adf1b3c
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-08-17 17:38:49 +0530

    Example

---
+++ Lecture 7/StringBuffer.java
@@ -0,0 +1,20 @@
+//GrowIND
+//Introduction to Java
+//Lecture 7: Arrays
+
+
+package com.company;
+import java.util.Scanner;
+
+
+class Main {
+
+    public static void main(String args[]){
+       StringBuffer str1 = new StringBuffer("I live in Ajmer");
+       String str = new String("I am Indian");
+       str1.append(" in Rajasthan");
+       System.out.println(str1);
+
+
+    }
+}
