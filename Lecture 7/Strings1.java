0b6125e8f979f5ae7676e8651acfa3cae73a349a
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-08-17 17:23:04 +0530

    Example

---
+++ Lecture 7/Strings1.java
@@ -0,0 +1,24 @@
+//GrowIND
+//Introduction to Java
+//Lecture 7: Arrays
+
+
+package com.company;
+import java.util.Scanner;
+
+
+class Main {
+
+    public static void main(String args[]){
+       String str = new String();
+       Scanner s = new Scanner(System.in);
+       str = s.nextLine();
+       System.out.println(str);
+       System.out.println(str.charAt(0));
+
+       String str1 = "pqr";
+       System.out.println(str.equals(str1));
+
+
+    }
+}
