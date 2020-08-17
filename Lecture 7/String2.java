5ca388119336e0b7d4b7b64a42452c512c991b5e
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-08-17 17:42:34 +0530

    Example

---
+++ Lecture 7/String2.java
@@ -0,0 +1,42 @@
+//GrowIND
+//Introduction to Java
+//Lecture 7: Arrays
+
+
+package com.company;
+
+
+
+class Main {
+
+    public static void main(String[] args) {
+        String line = "You are Awesome";
+        int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+
+        line = line.toLowerCase();
+        for(int i = 0; i < line.length(); ++i)
+        {
+            char ch = line.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i'
+                    || ch == 'o' || ch == 'u') {
+                ++vowels;
+            }
+            else if((ch >= 'a'&& ch <= 'z')) {
+                ++consonants;
+            }
+            else if( ch >= '0' && ch <= '9')
+            {
+                ++digits;
+            }
+            else if (ch ==' ')
+            {
+                ++spaces;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("Digits: " + digits);
+        System.out.println("White spaces: " + spaces);
+    }
+}
